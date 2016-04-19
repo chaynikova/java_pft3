e25361db805c31281ec2ec0eeb676b3a92639430
chaynikova <chaynikova@gmail.com>
2016-04-19 16:11:22 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,25 @@ package pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
+        double len = 5;
+        double a=4;
+        double b=6;
+
         System.out.println("Hello World!");
+        System.out.println("Площадь квадрата со стороной " +len + " = " + area(len));
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b + " = " + area(a,b));
 
     }
 
-}
+        public static double area(double l)
+    {
+return l*l;
+    }
+
+    public static double area(double a, double b)
+    {
+        return a*b;
+    }
+
+    }
+
