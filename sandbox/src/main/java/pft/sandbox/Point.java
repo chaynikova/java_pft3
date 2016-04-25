a28179e99c82fe7913a8c8389b47f05618c32f21
chaynikova <chaynikova@gmail.com>
2016-04-25 13:47:14 +0300

    Task2.1 Создан класс Point

--- sandbox/src/main/java/pft/sandbox/Point.java
+++ sandbox/src/main/java/pft/sandbox/Point.java
@@ -8,11 +8,18 @@ public class Point {
   public double x;
   //вторая координата точки
   public double y;
+}
+/* public  Point (double x, double y){
 
-public Point (double x, double y){
-
- this.x = x;
+  this.x = x;
   this.y= y;
 }
-
+  public double distance (){
+    x = p.x-this.x;
+    y = p.y-this.y;
+    p1.x = Math.pow(p1.x,2);
+    p2.y = Math.pow(p2.y,2);
+    double diff = p1.x+p2.y;
+    return Math.sqrt(diff);
  }
+*/
--- sandbox/src/main/java/pft/sandbox/Task2.java
+++ sandbox/src/main/java/pft/sandbox/Task2.java
@@ -6,19 +6,13 @@ package pft.sandbox;
 public class Task2 {
 
   public static void main(String[] args){
-    Point p1 = new Point(1,1);
-    Point p2 = new Point(2,2);
+    //Point p1 = new Point(1,1);
+  //  Point p2 = new Point(2,2);
 
-    System.out.println("Points coordinates: \nx1 = " +  p1.x + " and y1 = " + p1.y  + "\nx2 = " + p2.x + " and y2 = " + p2.y  + "\nThe distance is: "  + distance (p1,p2));
-      }
+   // System.out.println("Points coordinates: \nx1 = " +  p1.x + " and y1 = " + p1.y  + "\nx2 = " + p2.x + " and y2 = " + p2.y  + "\nThe distance is: "  + p.distance ());
+   //   }
 
-  public static double distance (Point p1, Point p2){
-   p1.x = p2.x-p1.x;
-   p2.y = p2.y-p1.y;
-   p1.x = Math.pow(p1.x,2);
-   p2.y = Math.pow(p2.y,2);
-   double diff = p1.x+p2.y;
-   return Math.sqrt(diff);
+
 
   }
 }
