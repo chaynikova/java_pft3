7422fff9720ecef299875ab7c5efc8b2b102dc4a
chaynikova <chaynikova@gmail.com>
2016-04-25 16:50:28 +0300

    Добавлен конструктор для класса Point

--- sandbox/src/main/java/pft/sandbox/Task2.java
+++ sandbox/src/main/java/pft/sandbox/Task2.java
@@ -6,17 +6,23 @@ package pft.sandbox;
 public class Task2 {
 
  public static void main (String [] args){
+Point p1 = new Point(1, 1);
+Point p2 = new Point(2, 2);
+ //  p1.x = 1;
+//   p1.y = 1;
+  // p2.x = 2;
+ //  p2.y = 2;
 
-   double x1 = 1;
-   double x2 = 2;
-   double y1 = 1;
-   double y2 = 2;
+   //double x1 = 1;
+  // double x2 = 2;
+ //  double y1 = 1;
+ //  double y2 = 2;
 
-   System.out.println("Distance is: " + distance(x1, x2, y1, y2));
+   System.out.println("Distance is: " + distance(p1, p2));
  }
 
-  public static double distance (double x1, double x2, double y1, double y2){
+  public static double distance (Point p1, Point p2){
 
-    return Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+    return Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
   }
 }
