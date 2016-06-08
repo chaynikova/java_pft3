5c89a3355e4c4a9403c0b4551c2801c2797253e4
chaynikova <chaynikova@gmail.com>
2016-06-08 13:14:07 +0300

    Циклы. Классы Prime&PrimeTests.

---
+++ sandbox/src/test/java/pft/sandbox/PrimeTests.java
@@ -0,0 +1,25 @@
+package pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by t.chaynikova on 6/8/2016.
+ */
+public class PrimeTests {
+  @Test
+  public void testPrime(){
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+    @Test
+    public void testNonPrime(){
+      Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+    }
+
+  @Test(enabled = false)
+  public void testPrimeLong(){
+    long n =Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+}
