11342becff8c35796216495385f345fa63c6ca34
Tatyana Chaynikova <chaynikova@gmail.com>
2016-06-19 19:51:49 +0300

    Groups del, mod, cr - done

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -42,10 +42,10 @@ try (BufferedReader reader = new BufferedReader(new FileReader(new File("src\\te
   @Test(dataProvider = "validGroups")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after.size(), equalTo(before.size() + 1));
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
